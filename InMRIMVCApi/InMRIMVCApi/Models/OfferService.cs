380f93bd06ee67c5c0440f15cb2043e0e4f0437c
SantuTd <134501567+SantuTd@users.noreply.github.com>
2023-09-28 13:27:56 +0530

    MVC Project
    Section 2

---
+++ InMRIMVCApi/InMRIMVCApi/Models/OfferService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InMRIMVCApi.Models
+{
+    public class OfferService
+    {
+        private List<Product> Inventory { get; set; }
+
+        private void AddInventory()
+        {
+            var products = new Product("P1", 1000, "P1 desc");
+                           new Product("P2", 200, "P2 desc");
+                           new Product("P3", 400, "P3 desc");
+                            new Product("P4", 700, "P4 desc");
+                            new Product("P5", 600, "P5 desc");
+                            new Product("P6", 800, "P6 desc");
+
+
+            Inventory.Add(products);
+        }
+    }
+}
